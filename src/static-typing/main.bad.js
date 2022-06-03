d1fbd6b8bd37f2d75714ed5019b8985c169b7642
Shehriyar Qureshi <thatdevsherry@gmail.com>
2022-06-03 21:18:30 +0500

    feat(): add NOTE

--- src/static-typing/main.bad.js
+++ src/static-typing/main.bad.js
@@ -2,5 +2,9 @@
   const obj = { width: 10, height: 10 };
   const area = obj.width * obj.heigh;
 
+  // NOTE: accessing property that does not exist is VALID JS
+  // TS fails not because it's a syntax error, but a logical one
+  // aka "non-sensical"
+
   console.log("area", area);
 }
